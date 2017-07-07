cbcd321eab041919eefd6c29e8bd076732b57f7e
songnan <songnan@pdmi.cn>
2017-07-07 08:44:06 +0800

    首页、设置管理页部分代码提交。

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -4,7 +4,8 @@ export default {
   },
   data () {
     return {
-      switchTab:0
+      switchTab:0,
+      value2:0
     }
   },
   methods: {
--- src/view/settings/account/index.js
+++ src/view/settings/account/index.js
@@ -1,10 +1,43 @@
 export default {
   name: 'ViewSettingsAccount',
   data () {
-    return {
-    }
+      return {
+          account: '运营人员1',
+          studioName: '一秒世界',
+          URL: 'yimiaoshijie.hubpd.com',
+          catalog: '一秒世界',
+          formValidate: {
+              name: '',
+              mail: '',
+              tel: ''
+          },
+          ruleValidate: {
+              name: [
+                  { required: true, message: '姓名不能为空', trigger: 'blur' }
+              ],
+              mail: [
+                  { required: true, message: '邮箱不能为空', trigger: 'blur' },
+                  { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+              ],
+              tel: [
+                  { required: true, message: '电话不能为空', trigger: 'blur' }
+              ]
+          }
+      }
   },
   methods: {
+      handleSubmit (name) {
+          this.$refs[name].validate((valid) => {
+              if (valid) {
+                  this.$Message.success('提交成功!');
+              } else {
+                  this.$Message.error('表单验证失败!');
+              }
+          })
+      },
+      handleReset (name) {
+          this.$refs[name].resetFields();
+      }
   },
   created () {},
   mounted () {}
