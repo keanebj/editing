35907b43290282df4fd6e75f93aa62b742a5f453
songnan <songnan@pdmi.cn>
2017-07-11 17:41:38 +0800

    7-11 添加文章详情页、公告页面。

---
+++ src/view/notice/index.js
@@ -0,0 +1,21 @@
+import MainHeader from '@/components/mainHeader/index.vue'
+import MainFooter from '@/components/mainFooter/index.vue'
+export default {
+  name: 'Home',
+  components: {
+    MainHeader,
+    MainFooter
+  },
+  created() {
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {   
+
+  },
+  mounted () {
+  }
+}
