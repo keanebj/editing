51d7ca39a073adca72e807e3ddcad4d4f141ba47
wangyilynda <1223351187@qq.com>
2017-08-02 17:06:53 +0800

    分享调整

--- src/view/share/index.js
+++ src/view/share/index.js
@@ -2,12 +2,32 @@ import {
   mapState
 } from 'vuex'
 export default {
-  name: 'Article',
-  created() {},
+  name: 'share',
+  created() {
+    this.shareId = this.$route.query.id;
+    //ajax获得分享的内容
+    this.$http.get("/api/content/share/" + this.shareId)
+      .then((response) => {
+        if(response.data.status == 1){
+             this.title = response.data.content.title;
+             this.content = response.data.content.content;
+        }else{
+           this.$Notice.warning({
+                title: response.data.message,
+                desc: false
+            })
+        }
+      }, (error) => {
+        this.$Notice.warning({
+            title: error.data.message,
+            desc: false
+        })
+    });
+  },
   data() {
     return {
       type: 'notice',
-      noticeID: -1,
+      shareId: -1,
       title: '',
       content: ''
     }
@@ -22,12 +42,5 @@ export default {
     var span19 = document.querySelector(".ivu-col-span-19")
     span5.style.display = 'none';
     span19.className = "layout-content-warp ivu-col ivu-col-span-24";
-    this.noticeID = this.$route.query.id;
-    //ajax获得分享的内容
-    this.$http.get("/api/content/share/" + this.noticeID)
-      .then((response) => {
-        this.title = response.data.content.title;
-        this.content = response.data.content.content;
-      })
   }
 }
