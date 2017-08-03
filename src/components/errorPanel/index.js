f7276887d91bcf5c6d7db80e20f544b00dd0067f
songnan <songnan@pdmi.cn>
2017-08-03 19:10:51 +0800

    修改bug

---
+++ src/components/errorPanel/index.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'ComponentsErrorPanel',
+  data() {
+    return {}
+  },
+  props: {
+    // logOut: Function,
+    // goHome: Function,
+    // goAccount: Function,
+    // studioLogo: {
+    //   type: String,
+    //   default: ''
+    // },
+    // userName: {
+    //   type: String,
+    //   default: '熊老师'
+    // }
+  },
+  methods: {},
+  created() {},
+  mounted() {}
+}
