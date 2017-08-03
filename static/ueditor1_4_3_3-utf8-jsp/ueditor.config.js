a5cb898d3c87a7d1987a97aa43c5fbccb22a56d0
songnan <songnan@pdmi.cn>
2017-08-03 09:17:49 +0800

    添加广告管理页无数据图片

--- static/ueditor1_4_3_3-utf8-jsp/ueditor.config.js
+++ static/ueditor1_4_3_3-utf8-jsp/ueditor.config.js
@@ -20,8 +20,8 @@
      * window.UEDITOR_HOME_URL = "/xxxx/xxxx/";
      */
    // var URL = window.UEDITOR_HOME_URL || getUEBasePath();
-    var URL = window.UEDITOR_HOME_URL || "/static/ueditor1_4_3_3-utf8-jsp/";
-    // var URL = window.UEDITOR_HOME_URL || "/newmedia/static/ueditor1_4_3_3-utf8-jsp/";
+    // var URL = window.UEDITOR_HOME_URL || "/static/ueditor1_4_3_3-utf8-jsp/";
+    var URL = window.UEDITOR_HOME_URL || "/newmedia/static/ueditor1_4_3_3-utf8-jsp/";
     /**
      * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
      */
