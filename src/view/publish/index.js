5f1339d18ff2cf4b422a6d1ded1f2ad851d5f9d9
songnan <songnan@pdmi.cn>
2017-08-07 15:32:15 +0800

    修改文字

--- src/view/publish/index.js
+++ src/view/publish/index.js
@@ -41,7 +41,7 @@ export default {
       publishChannels: ['人民日报中央厨房'],
       publishLabels: {
         Notice: '公告',
-        College: '融媒体学院'
+        College: '中央厨房融媒体学院'
       },
       titleMaxCount:22,
       summaryMaxCount:60,
