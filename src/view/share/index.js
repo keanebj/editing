a584cac397532dca23444c557ebd755dd0d109ee
weizhenglin <weizhenglin@zving.com>
2017-08-07 16:14:22 +0800

    工作室名称添加

--- src/view/share/index.js
+++ src/view/share/index.js
@@ -23,12 +23,13 @@ export default {
     //ajax获得分享的内容
     this.$http.get("/api/content/share/" + this.shareId)
       .then((response) => {
-      	
-      	console.log(response)
         if(response.data.status == 1){
           this.noData=true;
-             this.title = response.data.content.title;
-             this.content = response.data.content.content;
+            this.title = response.data.content.title;
+            this.conInfo.channel = response.data.content.channel;
+            this.content = response.data.content.content;
+            this.conInfo.time = response.data.content.addtime;
+	          this.conInfo.author = response.data.content.author;
         }else{
           this.noData=false;
            this.$Notice.warning({
