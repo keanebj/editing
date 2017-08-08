63c6fe0c6bfa943d76356b8b28fcc088f3ad989c
wangyilynda <1223351187@qq.com>
2017-08-08 10:10:07 +0800

    去掉console

--- src/view/scroll/index.js
+++ src/view/scroll/index.js
@@ -27,7 +27,6 @@ export default {
 //  }
   },
   mounted () {
-    console.log(this.previewCon);
     this.$emit('element', [this.$refs.onscroll, this.$refs.scrollCon, this.$refs.scroll, this.$refs.setCon]);
   },
   updated () {  
