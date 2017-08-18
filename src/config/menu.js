ad827d75aea0d07ed7e9de91e5c534e9af2a9828
songnan <songnan@pdmi.cn>
2017-08-18 13:03:57 +0800

    隐藏内容审核、素材管理

--- src/config/menu.js
+++ src/config/menu.js
@@ -30,16 +30,18 @@ export default [{
       name: '广告管理',
       path: '/manage/ad',
       authority: 'Manage'
-    }, {
-      name: '内容审核',
-      path: '/manage/contentaudit',
-      authority: 'Manage'
-    }, {
-	    name: '素材管理',
-	    path: '/manage/material',
-	    match: ['SourceMaterialEnter'],
-	    authority:'Edit'
-	  }
+    }
+    // , {
+    //   name: '内容审核',
+    //   path: '/manage/contentaudit',
+    //   authority: 'Manage'
+    // }
+    // , {
+	  //   name: '素材管理',
+	  //   path: '/manage/material',
+	  //   match: ['SourceMaterialEnter'],
+	  //   authority:'Edit'
+	  // }
   ]
 }, {
   name: '设置',
