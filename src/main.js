63355c8199c98b9358d48b7d8caa2152adebd728
wangyilynda <1223351187@qq.com>
2017-08-30 16:21:01 +0800

    audio预览

--- src/main.js
+++ src/main.js
@@ -19,24 +19,159 @@ import config from './config'
 Vue.prototype.$conf = config
 /* eslint-disable no-new */
 import app from './app'
-new Vue({
+var vueEle=new Vue({
   el: '#app',
   router,
   store,
   template: '<app/>',
-  components: { app }
+  components: { app },
+  methods:{     
+   //时间显示模式
+  _time(time) {
+    var minute = this.changeNum(Math.floor(time / 60));
+    var second = this.changeNum(Math.floor(time % 60));
+    var time = minute + ':' + second;
+    return time;
+  },
+  changeNum(num, n = 2) {
+    var len = num.toString().length;
+    while (len < n) {
+      num = '0' + num;
+      len++;
+    }
+    return num
+  }
+  }
 })
 
 //自定义指令--音频播放
-Vue.directive('my-directive', {
-  bind: function () {
-    alert(34234234);
+Vue.directive('my-directive-audio', {
+  bind: function (el,binding,vnode) {
     // 做绑定的准备工作
     // 比如添加事件监听器，或是其他只需要执行一次的复杂操作
+    if(binding.value){
+      el.innerHTML=binding.value;
+      //看是否有音乐播放器 
+      let audioArr=el.getElementsByClassName('audioWrap'); 
+      if(audioArr && audioArr.length > 0 ){
+          //添加播放事件
+          for (var item of audioArr) {
+            let timer=null;
+            //获得audio
+            let audio=item.getElementsByTagName('audio')[0];
+            let playimg=item.getElementsByTagName('img')[0];
+            let progress=item.getElementsByTagName('progress')[0];
+            let totleTimeDiv=item.getElementsByClassName('totleTime')[0];
+            let currentTimeDiv=item.getElementsByClassName('currentTime')[0];
+            
+            //添加点击事件
+            playimg.onclick=function(){
+                //获得时长
+                var totleTime = audio.duration;
+                setTimeout(function () {  
+                  if(isNaN(totleTime)){
+                    totleTime = audio.duration;
+                  }
+                  else{
+                    totleTimeDiv.innerHTML=vueEle._time(totleTime); 
+                  }
+                }, 100);
+                if(audio.paused){
+                  audio.play();
+                  //img 的图片
+                  playimg.src=playimg.getAttribute("src").replace('play.svg','playing.gif');
+                
+                  //刷新时间 
+                  clearInterval(timer);
+                    timer=setInterval(function(){
+                        var currentTime = audio.currentTime;
+                        currentTimeDiv.innerHTML=vueEle._time(currentTime);
+
+                        var currentTime = audio.currentTime;
+                        var totleTime = audio.duration;
+                        var percent = (currentTime / totleTime) * 100;
+                        progress.value=percent;
+
+                        if(currentTime ==  audio.duration){
+                            playimg.src=playimg.getAttribute("src").replace('playing.gif','play.svg');
+                            clearInterval(timer);
+                        } 
+                  },100)
+                }else{
+                    playimg.src=playimg.getAttribute("src").replace('playing.gif','play.svg');
+                    audio.pause();
+                    clearInterval(timer);
+                }
+          }
+        }   
+      }
+    }
+
   },
-  update: function (newValue, oldValue) {
+  inserted:function(el,binding){
+  },
+  update: function (el,binding) {
     // 根据获得的新值执行对应的更新
     // 对于初始值也会被调用一次
+   
+  },
+  componentUpdated:function(el,binding){
+    //被绑定元素所在模板完成一次更新周期时调用
+    el.innerHTML=binding.value;
+    //看是否有音乐播放器 
+    let audioArr=el.getElementsByClassName('audioWrap'); 
+    if(audioArr && audioArr.length > 0 ){
+        //添加播放事件
+        for (var item of audioArr) {
+          let timer=null;
+          //获得audio
+          let audio=item.getElementsByTagName('audio')[0];
+          let playimg=item.getElementsByTagName('img')[0];
+          let progress=item.getElementsByTagName('progress')[0];
+          let totleTimeDiv=item.getElementsByClassName('totleTime')[0];
+          let currentTimeDiv=item.getElementsByClassName('currentTime')[0];
+          
+          //添加点击事件
+          playimg.onclick=function(){
+              //获得时长
+              var totleTime = audio.duration;
+              setTimeout(function () {  
+                if(isNaN(totleTime)){
+                  totleTime = audio.duration;
+                }
+                else{
+                  totleTimeDiv.innerHTML=vueEle._time(totleTime); 
+                }
+              }, 100);
+              if(audio.paused){
+                 audio.play();
+                 //img 的图片
+                 playimg.src=playimg.getAttribute("src").replace('play.svg','playing.gif');
+               
+                 //刷新时间 
+                 clearInterval(timer);
+                  timer=setInterval(function(){
+                      var currentTime = audio.currentTime;
+                      currentTimeDiv.innerHTML=vueEle._time(currentTime);
+
+                      var currentTime = audio.currentTime;
+                      var totleTime = audio.duration;
+                      var percent = (currentTime / totleTime) * 100;
+                      progress.value=percent;
+
+                      if(currentTime ==  audio.duration){
+                          playimg.src=playimg.getAttribute("src").replace('playing.gif','play.svg');
+                          clearInterval(timer);
+                      } 
+                },100)
+              }else{
+                  playimg.src=playimg.getAttribute("src").replace('playing.gif','play.svg');
+                  audio.pause();
+                  clearInterval(timer);
+              }
+        }
+      }   
+    }
   },
   unbind: function () {
     // 做清理工作
