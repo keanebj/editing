e37da934bf56675ab698c9e3d568e0f749a85f39
wangyilynda <1223351187@qq.com>
2017-09-28 17:49:23 +0800

    登录模糊

--- src/view/publishtext/index.js
+++ src/view/publishtext/index.js
@@ -434,14 +434,6 @@ export default {
             var scale = ele[3].clientHeight / ele[0].clientHeight;
             ele[1].style.height = ele[2].clientHeight * scale + 'px'
           }
-          // for (var i = 0; i<$('.download').size(); i++) {
-          //   $('.download').eq(i).attr('href', $('.audioWrap.myDirectiveAudio').eq(i).attr('audio-url'))
-          //   if (navigator.userAgent.indexOf('Firefox') > -1 && $('.download').eq(i).attr('href')[0].indexOf(window.location.host) == -1) {
-          //     $('.download').eq(i).html('右键点击另存为下载文件！');
-          //   }else{
-          //     $('.download').eq(i).html('下载音频');
-          //   }
-          // }
         }, 500)
       } else {
         //    	不显示预览
@@ -538,6 +530,11 @@ export default {
             This.handleClick(e, '', 'linkimg');
           }, 500)
 
+        },(error)=>{
+          this.$Notice.error({
+            title: error.data.message,
+            desc: false
+          })
         })
       } else {
         this.noSel = true;
